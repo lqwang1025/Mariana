43b156970f83323ca762863ab51b0e45c9361aa1
lqwang1025 <787719091@qq.com>
2022-12-02 07:51:10 +0800

    Init

--- mariana/core/tensor_imp.cpp
+++ mariana/core/tensor_imp.cpp
@@ -2,13 +2,13 @@
  *        (C) COPYRIGHT Daniel Wang Limited.
  *             ALL RIGHTS RESERVED
  *
- * File       : tensor.cpp
+ * File       : core/tensor.cpp
  * Authors    : lqwang@pandora
  * Create Time: 2022-11-29:20:49:22
  * Description:
  * 
  */
 
-#include <core/tensor.h>
+#include <core/tensor_imp.h>
 #include <core/macros/macros.h>
 #include <core/utils/arrary_ref.h>
--- mariana/core/tensor_imp.h
+++ mariana/core/tensor_imp.h
@@ -2,24 +2,24 @@
  *        (C) COPYRIGHT Daniel Wang Limited.
  *             ALL RIGHTS RESERVED
  *
- * File       : core/tensor.h
+ * File       : core/tensor_imp.h
  * Authors    : lqwang@pandora
  * Create Time: 2022-11-29:20:49:13
  * Description:
  *
  */
 
-#ifndef __CORE_TENSOR_H__
-#define __CORE_TENSOR_H__
+#ifndef __CORE_TENSOR_IMP_H__
+#define __CORE_TENSOR_IMP_H__
 
 namespace mariana {
 
-class Tensor {
+class TensorImp {
 public:
-    Tensor();
-    ~Tensor();
+    TensorImp();
+    ~TensorImp();
 };
 
 } // namespace mariana
-#endif /* __CORE_TENSOR_H__ */
+#endif /* __CORE_TENSOR_IMP_H__ */
 
